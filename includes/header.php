5c481e63d65aa8d4a383ca8b2348671694743180
PAtel18751 <mind59024@gmail.com>
2026-05-29 14:06:26 +0545

    Add favicon from brand logo

--- includes/header.php
+++ includes/header.php
@@ -39,6 +39,8 @@ if ($user) {
     }
   }
   </script>
+  <link rel="icon" type="image/png" href="<?= APP_URL ?>/favicon.png">
+  <link rel="shortcut icon" href="<?= APP_URL ?>/favicon.png">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
