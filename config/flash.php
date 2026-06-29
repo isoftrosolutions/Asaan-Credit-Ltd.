172c7dbb39ca465d06997ee56126930f014f2e44
PAtel18751 <mind59024@gmail.com>
2026-06-29 16:31:30 +0545

    Add flash_success/error/info/warning convenience functions

--- config/flash.php
+++ config/flash.php
@@ -13,6 +13,11 @@ function flash_has(string $key): bool {
     return isset($_SESSION['_flash'][$key]);
 }
 
+function flash_success(string $msg): void { flash_set('success', $msg); }
+function flash_error(string $msg): void { flash_set('error', $msg); }
+function flash_info(string $msg): void { flash_set('info', $msg); }
+function flash_warning(string $msg): void { flash_set('warning', $msg); }
+
 function flash_render(): void {
     foreach (['success', 'error', 'info', 'warning'] as $type) {
         $msg = flash_get($type);
