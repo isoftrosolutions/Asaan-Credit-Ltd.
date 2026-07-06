31b8ef13776809b6e29363dd1f081fb3ccb9192b
PAtel18751 <mind59024@gmail.com>
2026-07-06 11:13:11 +0545

    Add YouTube channel link to hero section and footer social links

--- pages/index.php
+++ pages/index.php
@@ -627,6 +627,10 @@ require __DIR__ . '/../includes/header.php';
             <span>Watch</span>
           </button>
           <?php endif; ?>
+          <a href="https://www.youtube.com/@asaan.capital" target="_blank" rel="noopener" class="hp-hero-play" aria-label="YouTube channel" style="text-decoration:none;">
+            <svg viewBox="0 0 24 24" fill="currentColor" width="20" height="20"><path d="M23.5 6.19a3.02 3.02 0 00-2.12-2.14C19.5 3.5 12 3.5 12 3.5s-7.5 0-9.38.55A3.02 3.02 0 00.5 6.19 31.6 31.6 0 000 12a31.6 31.6 0 00.5 5.81 3.02 3.02 0 002.12 2.14c1.88.55 9.38.55 9.38.55s7.5 0 9.38-.55a3.02 3.02 0 002.12-2.14A31.6 31.6 0 0024 12a31.6 31.6 0 00-.5-5.81zM9.55 15.57V8.43L15.82 12l-6.27 3.57z"/></svg>
+            <span>YouTube</span>
+          </a>
         </div>
     </div>
   </div>
